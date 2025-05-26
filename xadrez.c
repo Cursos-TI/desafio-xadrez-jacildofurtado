6d4f9010478ce5f99d8d63146dc5f853a8188113
Francisco Jacildo Furtado da Silva <102054158+jacildofurtado@users.noreply.github.com>
2025-05-26 22:51:44 +0000

    Feito os movimentos da peças: Bispo, Torre e Rainha.

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
 
 // Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    // Variáveis de inicialização da movimentação das peças
+    int movimentoBispo = 0;
+    int movimentoTorre = 0;
+    int movimentoRainha = 0;
 
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("Movimentação do Bispo\n");
+
+    while (movimentoBispo < 5) { 
+        // Move o Bispo na diagonal superior direita 5 casas
+        printf("Cima Direita\n");
+        movimentoBispo++;
+    }
+
+    printf("\n"); // Deixa uma linha em branco
 
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("Movimentação da Torre\n");
+
+    do {
+        // Move a torre para a direita 5 casas
+        printf("Direita\n");
+        movimentoTorre++;
+    } while (movimentoTorre < 5);
+
+    printf("\n"); // Deixa uma linha em branco
 
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("Movimentação da Rainha\n");
+
+    for(movimentoRainha; movimentoRainha < 8; movimentoRainha++) { 
+        // Move a Rainha 8 para a esquerda 8 casa
+        printf("Esquerda\n");
+    }
+
+
+
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
